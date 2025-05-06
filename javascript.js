549b0f31ac2ab50604d452f8079040166212c154
Sam Moore <samhmoore95@gmail.com>
2025-05-06 06:16:12 -0400

    Finish Etch a Sketch portion.
    The etch a sketch is functionally working. Now I am going to add a
    button for users to reset the etch a sketch and enter their number of
    squares on the canvas.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 const container = document.querySelector(".container");
-const numOfBoxes = 16;
+const squaresPerSide = 80;
+const rows = squaresPerSide;
+const columns = squaresPerSide;
+const numOfBoxes = squaresPerSide * squaresPerSide;
+const boxHeight = (1000 / rows) - .25;
+const boxWidth = (1000 / columns) - .25;
 const divs = {};
+
 
 
 let i = 0;
@@ -8,6 +14,7 @@ while (i < numOfBoxes) {
     const newDiv = document.createElement('div');
     newDiv.classList.add("box-div");
     newDiv.id = `div${i}`;
+    newDiv.style.cssText = `width: ${boxWidth}px; height: ${boxHeight}px; border: solid; border-width: 1px`;
     container.appendChild(newDiv);
     divs[`div${i}`] = newDiv;
     i++;
